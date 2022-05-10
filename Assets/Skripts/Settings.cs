9ff2ec788d2176f522dd3c22ddf9ccd845ce850f
IQueue <aleva@sfedu.ru>
2022-05-10 17:52:54 +0300

    added Lock FPS button

--- Assets/Skripts/Settings.cs
+++ Assets/Skripts/Settings.cs
@@ -7,7 +7,7 @@ using System.Linq;
 public class Settings : MonoBehaviour
 {
     public Dropdown dropdown;
-
+    
     // Start is called before the first frame update
 
     string[] resolutions = { "1920x1080", "2560x1440", "3840x2160" };
@@ -25,8 +25,30 @@ public class Settings : MonoBehaviour
 
     public void SetQuality()
     {
-        Debug.Log("Устанавливаем разрешение");
+        Debug.Log("????????????? ??????????");
         string[] res = resolutions[dropdown.value].Split("x");
         Screen.SetResolution(int.Parse(res[0]), int.Parse(res[1]),true);
     }
+
+    bool f = true;
+
+    public void LockFPS ()
+    {
+        if (f)
+        {
+            Debug.Log("FPS locked at 60 value");
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
+            f = false;
+        }
+        else
+        {
+            Debug.Log("FPS unlocked");
+            QualitySettings.vSyncCount = 4;
+            Application.targetFrameRate = 1000;
+            f = true;
+        }
+    }
+
+
 }
